2846f6ab6fdf0ae858793cec099f906849402576
alinawaz <ali0578600@gmail.com>
2026-01-30 20:49:22 +0500

    projects updated

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -39,6 +39,34 @@ const Projects = ({ id }) => {
         <div id={id} className='overflow-hidden min-h-screen  w-screen flex flex-col justify-center  items-center bg-[#313849]' >
             <h1 className='text-3xl projectHeading text-white md:text-[4vw] font-bold text-center py-10' >Our <span className='text-center text-[#00d8ff]' >Projects</span></h1>
             <div className='flex flex-wrap flex-col mx-5 md:mx-0 md:flex-row gap-5' >
+                <div onClick={() => navigate('https://significoagency.vercel.app/')} className="project w-[90vw] group relative mx-auto h-[25vh] md:w-[30vw] md:h-[30vh] text-white rounded-md overflow-hidden">
+                    <img
+                        className="w-full h-full object-cover object-top rounded-md transition-transform duration-200 group-hover:scale-105 cursor-pointer"
+                        src={assets.project5}
+                        alt=""
+                    />
+                    <div
+                        className="
+                        absolute inset-0
+                        flex flex-col justify-center
+                        p-5
+                        bg-linear-to-b from-[#006071] to-[#00d9ff51]
+                        opacity-0
+                        translate-y-full
+                        transition-all duration-300
+                        group-hover:opacity-100
+                        group-hover:translate-y-0
+                        pointer-events-none
+                        "
+                    >
+                        <h1 className="text-center text-2xl">Agency website</h1>
+                        <h1 className="text-center">I had clone the agencywebsite significo.com</h1>
+                        <div className='flex justify-center' >
+                            <a className='bg-red-600 pointer-events-auto px-5 py-1 rounded-md mt-5' target='_blank' href="https://foreverfrontend-phi.vercel.app/">open it </a>
+                        </div>
+                    </div>
+
+                </div>
                 <div onClick={() => navigate('https://foreverfrontend-phi.vercel.app/')} className="project w-[90vw] group relative mx-auto h-[25vh] md:w-[30vw] md:h-[30vh] text-white rounded-md overflow-hidden">
                     <img
                         className="w-full h-full object-cover object-top rounded-md transition-transform duration-200 group-hover:scale-105 cursor-pointer"
